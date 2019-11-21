1ee7eaaea6e518c58c5efb29e774180f17fddfbb
dfounderliu <dfounderliu@tencent.com>
2019-11-21 13:44:11 +0800

    fix login

--- serverless.js
+++ serverless.js
@@ -77,18 +77,21 @@ class TencentCamRole extends Component {
     })
   }
 
-  async getTempKey() {
+  async getTempKey(temp) {
     const that = this
 
-    try {
-      while ((await fs.readFileSync('./.env_temp', 'utf8', { flag: 'rx' })) == 0) {
-        await that.sleep(1000)
-      }
-    } catch (e) {
-      if (e.toString().match('no such file or directory')) {
-        await fs.writeFileSync('./.env_temp', '0', { flag: 'wx' })
-      } else {
-        while (!(await fs.existsSync('./.env_temp'))) {
+    if (temp) {
+      while (true) {
+        try {
+          const tencent_credentials_read = JSON.parse(await fs.readFileSync('./.env_temp', 'utf8'))
+          if (
+            Date.now() / 1000 - tencent_credentials_read.timestamp <= 5 &&
+            tencent_credentials_read.AppId
+          ) {
+            return tencent_credentials_read
+          }
+          await that.sleep(1000)
+        } catch (e) {
           await that.sleep(1000)
         }
       }
@@ -99,7 +102,10 @@ class TencentCamRole extends Component {
       try {
         const tencent = {}
         const tencent_credentials_read = JSON.parse(data)
-        if (Date.now() / 1000 - tencent_credentials_read.timestamp <= 6000) {
+        if (
+          Date.now() / 1000 - tencent_credentials_read.timestamp <= 6000 &&
+          tencent_credentials_read.AppId
+        ) {
           return tencent_credentials_read
         }
         const login = new TencentLogin()
@@ -131,13 +137,16 @@ class TencentCamRole extends Component {
   }
 
   async default(inputs = {}) {
-    inputs = mergeDeepRight(defaults, inputs)
-
+    // login
+    const temp = this.context.instance.state.status
+    this.context.instance.state.status = true
     let { tencent } = this.context.credentials
     if (!tencent) {
-      tencent = await this.getTempKey(tencent)
+      tencent = await this.getTempKey(temp)
       this.context.credentials.tencent = tencent
     }
+
+    inputs = mergeDeepRight(defaults, inputs)
 
     const cam = this.getCamClient(this.context.credentials.tencent, inputs.region)
     cam.sdkVersion = 'ServerlessComponent'
